0843e35e250d087fd2df7da34dfcd2d2e87a9b0a
NkululekoMbhele <nkululekombhele6017@gmail.com>
2022-09-17 17:13:53 +0200

    update homepage

---
+++ Pages/Clients/Client.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RapeCrisisCounsellorSystem.Pages.Clients
+{
+    public class ClientModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Counsellors/Counsellor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RapeCrisisCounsellorSystem.Pages.Counsellors
+{
+    public class CounsellorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
